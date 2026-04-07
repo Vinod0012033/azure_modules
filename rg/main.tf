dd51219306239001373c775acc68bb3dd0c2cb40
Vinod0012033 <Vinod0012033@outlook.com>
2026-04-07 15:03:02 +0000

    Refactor preconditions in resource group lifecycle

--- rg/main.tf
+++ rg/main.tf
@@ -1,19 +1,19 @@
 resource "azurerm_resource_group" "rg" {
- 
- lifecycle {
-   precondition {
-    condition = (local.resource_group_name <= 90)
-    error_message = "Resource group name must be 90 characters or less to comply with Azure"
-     
-   }
-   
-   precondition {
-    condition = contains(["dev", "qa", "prod"], lower(var.context.environment))
-    error_message = "Environment must be one of 'dev', 'qa', or 'prod'"
-    }
- }
- name     = local.resource_group_name
- location = var.context.location
 
- tags     = local.default_tags
+  lifecycle {
+    precondition {
+      condition     = length(local.resource_group_name) <= 90
+      error_message = "Resource group name must be <= 90 characters"
+    }
+
+    precondition {
+      condition     = contains(["dev", "qa", "prod"], lower(var.context.environment))
+      error_message = "Environment must be dev, qa, or prod"
+    }
+  }
+
+  name     = local.resource_group_name
+  location = var.context.location
+
+  tags = local.default_tags
 }
