9b8e30188d4210b734eb4ccaf1d38b516943fb4a
Vinod0012033 <Vinod0012033@outlook.com>
2026-04-07 15:20:25 +0000

    Remove instance and tags variable declarations
    Removed instance and tags variable declarations from local.tf.

--- rg/local.tf
+++ rg/local.tf
@@ -13,17 +13,9 @@ variable "context" {
   })
 }
 
-variable "instance" {
-  description = "Unique identifier (01, 02...)"
-  type        = string
-
   validation {
     condition     = length(var.instance) >= 2
     error_message = "Instance must be at least 2 characters"
   }
 }
 
-variable "tags" {
-  type    = map(string)
-  default = {}
-}
