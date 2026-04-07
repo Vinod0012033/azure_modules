6585555db48f96a45125aa261389cc27d3da39f9
Vinod0012033 <Vinod0012033@outlook.com>
2026-04-07 16:22:54 +0000

    Refactor output variables for resource group

--- rg/output.tf
+++ rg/output.tf
@@ -1,11 +1,14 @@
-output "name" {
-  value = azurerm_resource_group.rg.name
+output "id" {
+  value       = azurerm_resource_group.resource_group.id
+  description = "(string) The ID of the resource group."
 }
 
-output "id" {
-  value = azurerm_resource_group.rg.id
+output "name" {
+  value       = azurerm_resource_group.resource_group.name
+  description = "(string) The name of the resource group."
 }
 
 output "location" {
-  value = azurerm_resource_group.rg.location
+  value       = azurerm_resource_group.resource_group.location
+  description = "(string) The location of the resource group."
 }
